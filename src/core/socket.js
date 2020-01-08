7589e67819d0368a0fc36142da69dc1e704fc86c
romanmashkovskiy <ra.mashkovskiy@gmail.com>
2020-01-08 21:50:01 +0200

    chat update

--- src/core/socket.js
+++ src/core/socket.js
@@ -22,16 +22,41 @@ export default {
 
             this._users[id] = client;
 
+            client.broadcast.emit('action', {
+                type: 'USER_CONNECTED',
+                data: id
+            });
+
             console.log(`Socket (userId: ${ id }): Connection Succeeded.`);
 
             client.on('disconnect', () => {
+                client.broadcast.emit('action', {
+                    type: 'USER_DISCONNECTED',
+                    data: id
+                });
+
                 delete this._users[id];
-                console.log(`Socket (userId: ${ id }): Disconnected.`)
+                console.log(`Socket (userId: ${ id }): Disconnected.`);
             });
 
             client.on('action', (action) => {
-                if (action.type === 'SERVER/MESSAGE') {
-                    this._io.emit('action', { type: 'NEW_MESSAGE', data: action.data });
+                if (action.type === 'SERVER_OUTGOING_MESSAGE') {
+                    client.broadcast.emit('action', {
+                        type: 'INCOMING_MESSAGE',
+                        data: action.data
+                    });
+                }
+                if (action.type === 'SERVER_USER_IS_TYPING') {
+                    client.broadcast.emit('action', {
+                        type: 'USER_IS_TYPING',
+                        data: action.data
+                    });
+                }
+                if (action.type === 'SERVER_USER_STOP_TYPING') {
+                    client.broadcast.emit('action', {
+                        type: 'USER_STOP_TYPING',
+                        data: action.data
+                    });
                 }
             });
         });
